ba1712b4e58ae34355b82015eadd8ecf73461556
Nikola Naumovski <nikola.naumovski@partners.xarvio.com>
2025-12-27 12:00:01 +0100

    fix: fixing publishing of package

--- move-files.js
+++ move-files.js
@@ -1,13 +1,16 @@
 // move-files.js
-const fs = require("fs");
-const path = require("path");
+import fs from 'fs';
+import path from 'path';
+
+// Deriving the directory name from import.meta.url
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 // Define the paths of the files you want to move
-const distDir = path.join(__dirname, "dist");
+const distDir = path.join(__dirname, 'dist');
 const rootDir = __dirname;
 
 // List the files you want to move
-const filesToMove = ["index.js", "index.d.ts", "index.js.map"];
+const filesToMove = ['index.js', 'index.d.ts', 'index.js.map'];
 
 // Move each file
 filesToMove.forEach((file) => {
